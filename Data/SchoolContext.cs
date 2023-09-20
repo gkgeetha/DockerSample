02cc41d9ade9ca94299bbdfa688db254ea1fc7ee
Geetha G K <geethagk2003@yahoo.co.in>
2023-09-20 12:47:26 +0530

    Add project files.

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace myWebApp.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options) { }
+        public DbSet<Models.Student>? Students { get; set; }
+    }
+}
